34519e2c74fc63c511d7d0cbba5f80639660caf7
Keijiro Takahashi <keijiro@gmail.com>
2020-07-17 23:28:50 +0900

    Add different settings

--- Assets/WallFx/WallFx.hlsl
+++ Assets/WallFx/WallFx.hlsl
@@ -1,3 +1,5 @@
+#if 1
+
 #include "Packages/jp.keijiro.noiseshader/Shader/SimplexNoise2D.hlsl"
 
 float3 WallFx(float2 uv)
@@ -18,3 +20,26 @@ float3 WallFx(float2 uv)
 
     return _Color1.rgb * n1 + _Color2.rgb * n2;
 }
+
+#else
+
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Color.hlsl"
+
+float3 WallFx(float2 uv)
+{
+    uint seed = uv.y * _Param1 * 2;
+    float width = (Hash(seed    ) / 2 + 0.5) * _Param2;
+    float speed = (Hash(seed + 1) / 2 + 0.5) * _Param3;
+
+    float x = uv.x;
+    uint sel = x / width + (_LocalTime + 10) * speed;
+    float hue = Hash(sel * 2);
+    float val = Hash(sel * 2 + 1);
+
+    val = smoothstep(0.5, 1, val);
+
+    return HsvToRgb(float3(hue, 1, val));
+}
+
+#endif
